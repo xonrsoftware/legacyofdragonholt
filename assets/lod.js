92412b71c783ee412b9b8ca2b13e6425246a4567
AVX <avx@xonr.ru>
2018-07-02 19:00:09 +0300

    Fix browser multiply back url history entries on incorrect entry open attempts.

--- assets/lod.js
+++ assets/lod.js
@@ -9,19 +9,15 @@
 });
 
 function MoveToEntry(newEntryid) {
-    setValues(newEntryid);
-    // pushState gives double history for back button
-    /*if (history.pushState) {
-        // IE10, Firefox, Chrome, etc.
-        window.history.pushState(null, null, '#' + newEntryid);
-    } else {
-        // IE9, IE8, etc
-        window.location.hash = '#' + newEntryid;
-    }*/
-    if (newEntryid != "1") {
-        window.location.hash = '#' + newEntryid;
-        document.getElementById("savefield").innerHTML = 'Сохранить текущую страницу: <span class="selecttxt" contenteditable="true">https://phenomen.github.io/legacyofdragonholt/#' + newEntryid + '</span>';
-    } else document.getElementById("savefield").innerHTML = "";
+    try {
+        setValues(newEntryid);
+        if (newEntryid != "1") {
+            window.location.hash = '#' + newEntryid;
+            document.getElementById("savefield").innerHTML = 'Сохранить текущую страницу: <span class="selecttxt" contenteditable="true">https://phenomen.github.io/legacyofdragonholt/#' + newEntryid + '</span>';
+        } else document.getElementById("savefield").innerHTML = "";
+    } catch (e) {
+        console.log("Error:" + e);
+    }
 }
 var currenthash;
 $(window).on('hashchange', function () {
@@ -35,13 +31,10 @@ $(window).on('hashchange', function () {
 
 function setValues(newEntry) {
     if (response[newEntry] == undefined) {
-        window.location.hash = '#' + currenthash;
         throw "No Data";
     }
     currenthash = newEntry;
-
     document.getElementById("story").innerHTML = response[newEntry].s;
-
     for (var i = 1; i < 7; i++) {
         if ("ov" + i in response[newEntry]) {
             document.getElementById("story").innerHTML += "<br>";
@@ -59,7 +52,7 @@ function setValues(newEntry) {
             if ("op" + i in response[newEntry]) {
                 document.getElementById("story").innerHTML += '<div class="movetext">' + response[newEntry]["op" + i] + '</div>';
             }
-            document.getElementById("story").innerHTML += '<a class="movelink" href="#' + response[newEntry]["ov" + i] + '">➥ ' + response[newEntry]["ot" + i] + '.</a>';
+            document.getElementById("story").innerHTML += '<a class="movelink" href="#' + response[newEntry]["ov" + i] + '" onclick="MoveToEntry(\'' + response[newEntry]["ov" + i] + '\');return false;">➥ ' + response[newEntry]["ot" + i] + '.</a>';
         }
 
     }
